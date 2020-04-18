5ef2bfe309c5c87083d96de5b5b012acac85d098
TakeMyHand <prey4@TakeMyHands-MacBook-Air.local>
2020-04-19 01:13:51 +0530

    Fixed add domain

--- recon-bot/listener.py
+++ recon-bot/listener.py
@@ -9,20 +9,6 @@ alerter = Alerter(os.environ["FB_ACCESS_TOKEN"], '666434090858702')
 
 app = Flask(__name__)
 
-@app.route('/add-domain', methods=['POST'])
-def add():
-    if request.method == 'POST':
-        domain = request.args['text']
-        print(domain)
-        response = alerter.addDomain(domain)
-        if response == True:
-            slack.chat.post_message('#subdomain-alerts', "Successfully added " + domain)
-        else:
-            slack.chat.post_message('#subdomain-alerts', "Could not add " + domain)
-        return response, 200
-    else:
-        print("Failed")
-
 @app.route('/list-domains', methods=['POST'])
 def list():
     if request.method == 'POST':
@@ -31,13 +17,25 @@ def list():
             slack.chat.post_message('#subdomain-alerts', domain['domain'])
         return '', 200
 
+@app.route('/add-domain', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        response = alerter.addDomain(request.form['text'])
+        if response is True:
+            slack.chat.post_message('#subdomain-alerts', "Successfully added " + domain)
+            return '', 200
+        else:
+            slack.chat.post_message('#subdomain-alerts', "Could not add " + domain)
+            return 'Error', 200
+    else:
+        print("Failed")
+
 @app.route('/subdomain-alert', methods=['GET', 'POST'])
 def webhook():
     if request.method == 'GET':
         return str(request.args['hub.challenge']), 200
     elif request.method == 'POST':
         print(request.json[''])
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
