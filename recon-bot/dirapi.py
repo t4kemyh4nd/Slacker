615f43fb646610397137f649fd45a715246d0f2b
TakeMyHand <prey4@TakeMyHands-MacBook-Air.local>
2020-04-23 01:47:53 +0530

    Added directory alerts

---
+++ recon-bot/dirapi.py
@@ -0,0 +1,75 @@
+import schedule
+import time
+import os
+import pymongo
+import json
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["dirscan"]
+col = db["domains"]
+
+def scanDirs(domain):
+    if "http://" in domain:
+        filename = str(time.time())
+        os.system("touch /tmp/" + filename)
+        os.system("python3 /Users/prey4/Pentesting/dirsearch/dirsearch.py -u " + domain + " --json-report=/tmp/" + filename + " -e * --threads 200")
+        with open("/tmp/" + filename, 'r') as f:
+            results = json.load(f)
+            paths = []
+            for result in results[domain + ":80/"]:
+                if result["status"] not in [400]:
+                    paths.append(result["path"])
+            col.insert({"domain": domain, "paths": paths}, check_keys = False)
+        os.remove("/tmp/" + filename)
+    elif "https://" in domain:
+        filename = str(time.time())
+        os.system("touch /tmp/" + filename)
+        os.system("python3 /Users/prey4/Pentesting/dirsearch/dirsearch.py -u " + domain + " --json-report=/tmp/" + filename + " -e * --threads 200")
+        with open("/tmp/" + filename, 'r') as f:
+            results = json.load(f)
+            paths = []
+            for result in results[domain + ":443/"]:
+                if result["status"] not in [400]:
+                    paths.append(result["path"])
+            col.insert({"domain": domain, "paths": paths}, check_keys = False)
+        os.remove("/tmp/" + filename)
+
+class DirAlert:
+    def __init__(self, domain):
+        self.domain = domain
+        scanDirs(self.domain)
+
+    def compareResults(self):
+        oldPaths = col.find({"domain": self.domain}, {"_id": 0, "paths": 1})
+        if "http://" in self.domain:
+            filename = str(time.time())
+            os.system("touch /tmp/" + filename)
+            os.system("python3 /Users/prey4/Pentesting/dirsearch/dirsearch.py -u " + self.domain + " --json-report=/tmp/" + filename + " -e * --threads 200")
+            with open("/tmp/" + filename, 'r') as f:
+                results = json.load(f)
+                newPaths = []
+                for result in results[self.domain + ":80/"]:
+                    if result["status"] not in [400]:
+                        newPaths.append(result["path"])
+            os.remove("/tmp/" + filename)
+        elif "https://" in self.domain:
+            filename = str(time.time())
+            os.system("touch /tmp/" + filename)
+            os.system("python3 /Users/prey4/Pentesting/dirsearch/dirsearch.py -u " + self.domain + " --json-report=/tmp/" + filename + " -e * --threads 200")
+            with open("/tmp/" + filename, 'r') as f:
+                results = json.load(f)
+                paths = []
+                for result in results[self.domain + ":443/"]:
+                    if result["status"] not in [400]:
+                        newPaths.append(result["path"])
+            os.remove("/tmp/" + filename)
+        print("old paths: " + str(oldPaths))
+        print("new paths: " + str(newPaths))
+        
+    def createAlerts(self):
+        #schedule.every().day.at("10:30").do(self.compareResults(domain))
+        schedule.every(2).minutes.do(self.compareResults, self.domain)
+
+
+siteAlerter =  DirAlert("http://blog.takemyhand.xyz")
+siteAlerter.compareResults()
