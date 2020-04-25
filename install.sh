bbc49c6f9c866b5bdbaa037fab2a0c6329271cd4
TakeMyHand <prey4@TakeMyHands-MacBook-Air.local>
2020-04-25 16:41:21 +0530

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -4,23 +4,26 @@ echo "Installing Slacker..."
 
 pip install -r requirements.txt
 
-read -p "Enter facebook app ID" FB_APP_ID
-read -p "Enter facebook access token" FB_ACCESS_TOKEN
-read -p "Enter Slack bot token" SLACK_BOT_TOKEN
-read -p "Enter Slack webhook URL" SLACK_WEBHOOK_URL
+read -p "Enter facebook app ID: " FB_APP_ID
+read -p "Enter facebook access token: " FB_ACCESS_TOKEN
+read -p "Enter Slack bot token: " SLACK_BOT_TOKEN
+read -p "Enter Slack webhook URL: " SLACK_WEBHOOK_URL
 
-function addVar() {
-echo "Adding $1 to env variables..."
-echo $1 >> ~/.bashrc
-echo $1 >> ~/.profile
-echo $1 >> /etc/environment
-echo "Done..."
-}
+echo "export FB_APP_ID=$FB_APP_ID" >> ~/.bashrc
+echo "export FB_APP_ID=$FB_APP_ID" >> ~/.profile
+echo "export FB_APP_ID=$FB_APP_ID" >> /etc/enviroment
 
-addVar $FB_APP_ID
-addVar $FB_ACCESS_TOKEN
-addVar $SLACK_BOT_TOKEN
-addVar $SLACK_WEBHOOK_URL
+echo "export FB_ACCESS_TOKEN=$FB_ACCESS_TOKEN" >> ~/.bashrc
+echo "export FB_ACCESS_TOKEN=$FB_ACCESS_TOKEN" >> ~/.profile
+echo "export FB_ACCESS_TOKEN=$FB_ACCESS_TOKEN" >> /etc/environment
+
+echo "export SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL" >> ~/.bashrc
+echo "export SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL" >> ~/.profile
+echo "export SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL" >> /etc/environment
+
+echo "export SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN" >> ~/.bashrc
+echo "export SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN" >> ~/.profile
+echo "export SLACK_BOT_TOKEN=$SLACK_BOT_TOKEN" >> /etc/environment
 
 source ~/.bashrc
 source ~/.profile
