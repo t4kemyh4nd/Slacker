309d71928c92fee65c5e9ed49596fde3f8244467
TakeMyHand <prey4@TakeMyHands-MacBook-Air.local>
2020-04-25 18:06:51 +0530

    Updated requirements

--- install.sh
+++ install.sh
@@ -10,6 +10,6 @@ read -p "Enter Slack bot token: " SLACK_BOT_TOKEN
 read -p "Enter Slack webhook URL: " SLACK_WEBHOOK_URL
 read -p "Enter full path of dirsearch python file (eg. /users/dirsearch/dirsearch.py): " DIRSEARCH_PATH
 
-echo "{\"FB_APP_ID\": \"$FB_APP_ID\", \"FB_ACCESS_TOKEN\": \"$FB_ACCESS_TOKEN\", \"SLACK_BOT_TOKEN\": \"$SLACK_BOT_TOKEN\", \"SLACK_WEBHOOK_URL\": \"$SLACK_WEBHOOK_URL\", \"DIRSEARCH_PATH\": \"$DIRSEARCH_PATH\"}" > ./config.json
+echo "{\"FB_APP_ID\": \"$FB_APP_ID\", \"FB_ACCESS_TOKEN\": \"$FB_ACCESS_TOKEN\", \"SLACK_BOT_TOKEN\": \"$SLACK_BOT_TOKEN\", \"SLACK_WEBHOOK_URL\": \"$SLACK_WEBHOOK_URL\", \"DIRSEARCH_PATH\": \"$DIRSEARCH_PATH\"}" > ./recon-bot/config.json
 
 echo "Done..."
